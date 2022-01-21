b29def2e475b7b060b41591af27b6a97468a4194
rimirim <rimizzaang@naver.com>
2022-01-22 00:42:37 +0900

    등비수열 추가

---
+++ src/Sequence/EqualRatio.java
@@ -0,0 +1,25 @@
+package Sequence;
+import java.math.BigInteger;
+
+public class EqualRatio {
+
+    // 등비수열 1, 2, 4, 8, 16, 32, 64 ...
+    // BigInteger을 통해 50번째 항을 구해보자
+
+    int ratio;
+
+    public EqualRatio(final int ratio) {
+
+        this.ratio = ratio;
+
+    }
+
+    public BigInteger getSeqData(final int count) {
+
+        for (int i = 0; i < count; i++) {
+            System.out.printf("%d^%d = %d\n", ratio, i,
+                    BigInteger.valueOf(ratio).pow(i));
+        }
+        return BigInteger.valueOf(ratio).pow(count - 1);
+    }
+}
--- src/Sequence/MySequenceTest.java
+++ src/Sequence/MySequenceTest.java
@@ -6,15 +6,18 @@ import Sequence.Rule_Sequence;
 public class MySequenceTest {
         public static void main(String[] args) {
 
+
             int [] fiboArr = {1,1};
             Fibo_Sequence fibo = new Fibo_Sequence (fiboArr);
-
             fibo.getData(20);
 
             int [] ruleArr = {1, 1, 1};
             Rule_Sequence rule = new Rule_Sequence(ruleArr);
-
             rule.getData(25);
+
+            EqualRatio er = new EqualRatio(2);
+            er.getSeqData(50);
+
 
         }
     }
