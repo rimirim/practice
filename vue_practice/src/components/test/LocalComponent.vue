3679ac153fe98f1d4ba9e4660319e586daae24ef
rimirim <rimizzaang@naver.com>
2022-03-04 17:11:12 +0000

    Component 이해하기 Local

---
+++ vue_practice/src/components/test/LocalComponent.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        {{ num }}
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'local-component',
+    props: {
+        num: Number
+    }
+}
+</script>
