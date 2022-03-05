a0ed4b243f49804508267298c6a1fa520ee2d4a4
rimirim <rimizzaang@naver.com>
2022-03-05 18:23:13 +0000

    스프링과 연동 1 (board 연결)

--- vue_practice/src/router/index.js
+++ vue_practice/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import TestView from '@/views/TestView.vue'
+import BoardListPage from '@/views/board/BoardListPage.vue'
 
 Vue.use(VueRouter)
 
@@ -15,6 +16,11 @@ const routes = [
     path: '/test',
     name: 'Test',
     component: TestView
+  },
+  {
+    path: '/boardlist',
+    name: 'BoardListPage',
+    component: BoardListPage
   }
 ]
 
---
+++ vue_practice/src/views/board/BoardListPage.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="board">
+        <h2> Vue + Spring 게시판 </h2>
+        <p> 게시물 작성 </p>
+        <board-list :board="boards"/>
+    </div>
+</template>
+
+<script>
+
+import BoardList from '@/components/board/BoardList.vue'
+export default {
+    name: 'BoardListPage',
+    components: {
+        BoardList
+    }
+}
+</script>
