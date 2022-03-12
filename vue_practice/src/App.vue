5ba787f6c48922670017c99e2ed4ef4a97d4695f
rimirim <rimizzaang@naver.com>
2022-03-12 15:09:08 +0900

    Vue Practice 1 - Test페이지 만들기

--- vue_practice/src/App.vue
+++ vue_practice/src/App.vue
@@ -1,14 +1,13 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+  <v-app>
+    <div id="app">
+      <router-view/>
+    </div>
+  </v-app>
 </template>
 
 <style>
+/*
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -29,4 +28,5 @@ nav a {
 nav a.router-link-exact-active {
   color: #42b983;
 }
+*/
 </style>
