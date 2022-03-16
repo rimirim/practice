f4a539f00f149f1a407c99fc381c9e4f31cb0510
rimirim <rimizzaang@naver.com>
2022-03-16 21:01:48 +0900

    Vue CoverFlow

---
+++ vue_practice/src/views/ui/CoverFlowTest.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <coverflow :coverList="coverList" :coverWidth="260" :index="2"/>
+    </div>
+</template>
+
+<script>
+
+import coverflow from 'vue-coverflow'
+
+export default {
+    name: 'CoverflowTestPage',
+    data () {
+        return {
+            coverList: [
+                {
+                    cover: require('@/assets/logo.png'),
+                    title: '듄'
+                },
+                 {
+                    cover: require('@/assets/logo.png'),
+                    title: '듄'
+                },
+                 {
+                    cover: require('@/assets/logo.png'),
+                    title: '듄'
+                },
+                 {
+                    cover: require('@/assets/logo.png'),
+                    title: '듄'
+                }
+              
+            ]
+        }
+    },
+    components: {
+        coverflow
+    }
+}
+
+</script>
