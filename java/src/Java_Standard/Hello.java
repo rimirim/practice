60ff5c0d5d53fdd2f60cb4f82a577943133937f8
rimirim <rimizzaang@naver.com>
2022-03-22 00:17:42 +0900

    ect

---
+++ java/src/Java_Standard/Hello.java
@@ -0,0 +1,17 @@
+package Java_Standard;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, world");
+        // ctrl + D : 한 줄 추가
+        // ctrl + 마우스 휠 : 폰트 사이즈 조절
+        // tab : 들여쓰기
+        // shift + tab : 반대로 들여쓰기
+        // alt + insert : Generate
+        // ctrl + shift + i : 코드 구현부 즉시 보기기
+        // 코드 드래그 + ctrl + shift + i : 코드 정렬
+        // alt + shift + f10 : 실행
+        // alt + 화살표 좌우 : 탭 이동
+
+
+}
